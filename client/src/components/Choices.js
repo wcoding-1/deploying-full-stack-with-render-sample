9f36ed087d649663ca0f83586a6c70ddeb1e05ca
Abrahim Kaba <167114548+wcoding-1@users.noreply.github.com>
2024-08-15 18:40:17 +0900

    button

--- client/src/components/Choices.js
+++ client/src/components/Choices.js
@@ -5,7 +5,7 @@ const Choices = ({ handleNewActivity, handleAddActivity, name }) => {
   return (
     <div>
       <button id="primary-btn" onClick={() => handleNewActivity()}>No thanks...</button>
-      <button id="success-btn" onClick={() => handleAddActivity(name)}>Sounds fun!</button>
+      <button id="success-btn" onClick={() => handleAddActivity(name)}>Sounds very fun!</button>
       
     </div>
   )
